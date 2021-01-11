7554848689b28a8107695df536c279a0f7be7f66
Luca Valsecchi <luca.valsecchi@hotelscan.com>
2021-01-11 17:56:42 +0800

    chore(rollup): add some new plugins and custom file for babel configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,32 @@
-import babel from "rollup-plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import replace from "@rollup/plugin-replace";
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
-import terser from "rollup-plugin-terser";
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import terser from 'rollup-plugin-terser';
+import progress from 'rollup-plugin-progress';
 
-const NODE_ENV = process.env.NODE_ENV || "development";
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 export default {
-  input: "./src/index.js",
+  input: './src/index.js',
   output: {
-    file: "./build/bundle.js",
-    format: "umd",
+    file: './build/bundle.js',
+    format: 'umd',
   },
   plugins: [
+    progress(),
     replace({
-      "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
     }),
     babel({
-      exclude: "node_modules/**",
+      exclude: 'node_modules/**',
     }),
     resolve(),
     commonjs(),
-    NODE_ENV !== "production" && serve({ contentBase: "build" }),
-    NODE_ENV !== "production" && livereload(),
-    NODE_ENV === "production" && terser()
+    NODE_ENV !== 'production' && serve({ contentBase: 'build' }),
+    NODE_ENV !== 'production' && livereload(),
+    NODE_ENV === 'production' && terser(),
   ],
 };
